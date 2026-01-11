7657a17cb2fffd4d6d44f55246a5645197d7847e
acboulet <39838871+acboulet@users.noreply.github.com>
2026-01-11 23:04:32 +0000

    update devcontainer for quarto

---
+++ .devcontainer/init-env.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script initializes the development environment inside the dev container.
+echo "Initializing development environment..."
+
+# Update package lists
+pip install --upgrade pip \
+    && pip install -r requirements.txt 
+echo "Python packages installed."
+
+  
+wget -qO- https://quarto.org/download/latest/quarto-linux-amd64.deb -O /tmp/quarto.deb
+sudo dpkg -i /tmp/quarto.deb
+rm /tmp/quarto.deb
+export QUARTO_PYTHON="/usr/local/python/current/bin/python3"
+echo "Quarto installed."
