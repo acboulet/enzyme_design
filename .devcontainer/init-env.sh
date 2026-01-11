7d82a30b3271401ca28e9812196550396b457afe
acboulet <39838871+acboulet@users.noreply.github.com>
2026-01-11 23:31:45 +0000

    adding lfs-hook to solve pull error

--- .devcontainer/init-env.sh
+++ .devcontainer/init-env.sh
@@ -4,11 +4,12 @@
 echo "Initializing development environment..."
 
 # Update package lists
+echo "Updating package lists..."
 pip install --upgrade pip \
     && pip install -r requirements.txt 
 echo "Python packages installed."
 
-  
+echo "Installing Quarto..."
 wget -qO- https://quarto.org/download/latest/quarto-linux-amd64.deb -O /tmp/quarto.deb
 sudo dpkg -i /tmp/quarto.deb
 rm /tmp/quarto.deb
